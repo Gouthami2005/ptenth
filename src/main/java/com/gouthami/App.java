a7f0cd1dea1276f7e250c96bd5b69d5d332eb5aa
Gouthami2005 <gouthamidoddamane21@gmail.com>
2025-06-08 14:19:30 +0530

    azure pipeline

---
+++ src/main/java/com/gouthami/App.java
@@ -0,0 +1,13 @@
+package com.gouthami;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
